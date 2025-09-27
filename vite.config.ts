ec8b6ad7a1f72de28a4c6e7ae95ff2d531ac329b
Delightech28 <desolomon07@gmail.com>
2025-09-27 14:00:06 +0100

    kh

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,9 @@ export default defineConfig(({ mode }) => ({
         }),
         NodeModulesPolyfillPlugin(),
       ],
+      define: {
+        'process.env': '{}',
+      },
     },
   },
 }));
